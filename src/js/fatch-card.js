a935ff5faca17ae670a4af62a4cebd1367a2a988
Siniavskay-Nadejda <87280720+Siniavskay-Nadejda@users.noreply.github.com>
2022-01-11 13:44:49 +0200

    сохраняет файлы

---
+++ src/js/fatch-card.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+export {fetchImages}
+axios.defaults.baseURL = 'https://pixabay.com/api/'
+const KEY = "25162138-7740bc78a7513a91b3e4ac5d0"
+
+async function fetchImages(query, page, perPage) {
+  const response = await axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+  )
+  return response
+}
